2ae726d540f57375a28e949db74fc193a7c4d8ca
Hasan <heshpulatov6@gmail.com>
2022-10-22 17:44:46 +0500

    arrayMethod

---
+++ js/script.js
@@ -0,0 +1,36 @@
+// homework 1
+
+// let b = [
+//     'Uzbekistan', 'AQSH', 'China', 'Russian', 'Germany', 'England', 'Belgium', 'Japan', 'Korea', 'Ecuador'
+// ]
+
+// for (let i = 0; i < b.length; i++) {
+//     console.log(i + " " + b[i]);
+// }
+
+// homework 2
+
+// let b = [
+//     'Uzbekistan', 'AQSH', 'China', 'Russian', 'Germany', 'England', 'Belgium', 'Japan', 'Korea', 'Ecuador', 1
+// ]
+
+// function word() {
+//     let a = String(b.length - 1);
+//     console.log(b.slice(0, a));
+// }
+
+// word()
+
+//homework 3
+
+// let b = [
+//     'Uzbekistan', 'AQSH', 'China', 'Russian', 'Germany', 'England', 'Belgium', 'Japan', 'Korea', 'Ecuador'
+// ]
+
+
+// function words(wordd) {
+//     let a = String(b.join(',') + "," + wordd)
+//     console.log(a.split(','));
+// }
+
+// words('Salom')
